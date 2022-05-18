463db06a40f4ef224ee46ebfde4ba74fa68422e9
Kate Martin <katejaneen23@gmail.com>
2022-05-18 09:07:47 +0800

    Restore email template

--- resources/views/mail/verification.blade.php
+++ resources/views/mail/verification.blade.php
@@ -1,6 +1,6 @@
 <h1>{{ config('app.name') }} - Login Verification</h1>
  
-<p>Hello there, {{ $name }}</p>
+<p>Hello there, {{ $firstName }}</p>
 <p>Your verification token is <strong>{{ $token }}</strong>.</p>
 
 Thank you!<br><br>
